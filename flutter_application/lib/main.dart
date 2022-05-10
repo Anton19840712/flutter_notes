c6c92a9f4ce7b71601e899509ae639ffa9c82f29
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-05-10 20:46:43 +0300

    Add stateless widget.

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(const MyFirstApp());
+
+class MyFirstApp extends StatelessWidget {
+  const MyFirstApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.greenAccent,
         appBar: AppBar(
-          title: const Text("My initial application"),
+          title: const Text("Logistics application"),
           centerTitle: true,
         ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                LinearProgressIndicator(
+                    value: 100, color: Colors.lightBlue, minHeight: 5),
+                Text(
+                  '23%',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                Text(
+                  'Load orders',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: const FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.cloud_download),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
